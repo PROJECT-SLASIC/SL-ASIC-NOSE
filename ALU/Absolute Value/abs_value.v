5e621b1faafe2cde428ccbd7e0e7b9e681f84bf4
cimonik <142085739+cimonik@users.noreply.github.com>
2023-09-02 17:50:10 +0300

    Update abs_value.v

--- ALU/Absolute Value/abs_value.v
+++ ALU/Absolute Value/abs_value.v
@@ -1,8 +1,9 @@
-module abs_value_ieee754 (
+module absolute_value (
     input [31:0] data_in,
+    input start,
     output [31:0] data_out
 );
 
-assign data_out = data_in & 32'h7FFFFFFF;
+assign data_out = start ? (data_in & 32'h7FFFFFFF) : 32'd0;
 
 endmodule
