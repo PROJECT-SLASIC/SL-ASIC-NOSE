324a39eb088113e760ddc9ebee577724b9d37a8a
cimonik <142085739+cimonik@users.noreply.github.com>
2023-09-02 17:57:42 +0300

    Update adder.v

--- ALU/Adder/adder.v
+++ ALU/Adder/adder.v
@@ -1,11 +1,10 @@
 module adder(
     input [31:0] A, B,
     input carry_in,
-    input start,
     output [31:0] sum,
     output carry_out
 );
 
-assign {carry_out, sum} = start ? (A + B + carry_in) : {1'b0, 32'b0};
+assign {carry_out, sum} = A + B + carry_in;
 
 endmodule
