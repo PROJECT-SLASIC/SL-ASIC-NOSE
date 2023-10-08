f1ef5180ee2c90f2979889d09442024ae7c57f2f
Ethem Buğra Arslan <101515029+LyraN66@users.noreply.github.com>
2023-10-08 09:43:32 +0300

    Create FIFO.v

---
+++ Peripherals/SPI/SPI_MASTER/FIFO.v
@@ -0,0 +1,41 @@
+module FIFO(
+    input clk,
+    input rst_n,
+    input wr_en,
+    input [7:0] wr_data,
+    input rd_en,
+    output reg [7:0] rd_data,
+    output empty,
+    output full
+    );
+    
+    parameter DEPTH = 516; // Default depth, can be changed as needed
+    
+    reg [7:0] mem [0:DEPTH-1];
+    reg [9:0] wr_ptr = 0;
+    reg [9:0] rd_ptr = 0;
+    reg [10:0] count = 0;
+    
+    always @(posedge clk) begin
+        if (rst_n) begin
+            wr_ptr <= 0;
+            rd_ptr <= 0;
+            count <= 0;
+        end else begin
+            if (wr_en && !full) begin
+                mem[wr_ptr] <= wr_data;
+                wr_ptr <= wr_ptr + 1'b1;
+                count <= count + 1'b1;
+            end
+            if (rd_en && !empty) begin
+                rd_data <= mem[rd_ptr];
+                rd_ptr <= rd_ptr + 1'b1;
+                count <= count - 1'b1;
+            end
+        end
+    end
+    
+    assign empty = (count == 0);
+    assign full = (count == DEPTH);
+
+endmodule
