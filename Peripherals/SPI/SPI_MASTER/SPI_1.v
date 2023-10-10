7b5aee707152e9ed2f7208a36005377e7cac4805
Ethem Buğra Arslan <101515029+LyraN66@users.noreply.github.com>
2023-10-10 19:49:12 +0300

    Update SPI_1.v

--- Peripherals/SPI/SPI_MASTER/SPI_1.v
+++ Peripherals/SPI/SPI_MASTER/SPI_1.v
@@ -1,4 +1,4 @@
-module SPI_1#(parameter width = 48)(
+module SPI_1(
     input clk,      // Internal Clock - 100 MHz - 10ns e3
     input rst,  // sw 1
     input clk_ss,   // 0 means 400_000Hz, 1 means 20_000_000Hz  sw0
@@ -8,7 +8,6 @@ module SPI_1#(parameter width = 48)(
     input [9:0] receive_data_length,            // represent in byte
     input [5:0] cmd_length,        // represent in byte
     input [9:0] send_data_length,            // represent in byte
-    input cmd_type,     // 0 for read 1 for write
     
     output reg CS,      // Chip Select - Active Low     JA 1
     output reg MOSI,    // Master out Slave in          JA 2
@@ -24,8 +23,8 @@ module SPI_1#(parameter width = 48)(
     reg [7:0] wr_data_fifo;
     reg rd_en_fifo;
     wire [7:0] rd_data_fifo;
-    wire empty_fifo;
-    wire full_fifo;
+    wire empty_fifo;                    // not used 
+    wire full_fifo;                     // not used 
     FIFO FIFO_1(
         . clk(SCK),
         . rst_n(rst),
@@ -66,7 +65,6 @@ module SPI_1#(parameter width = 48)(
                 clk_1 <= ~clk_1;
                 indicator <= 1'b1;
                 rst_reg = (indicator) ? (1'b0) : (1'b1);
-                
             end
             else begin
                 counter_1 <= counter_1 + 1;
@@ -86,16 +84,13 @@ module SPI_1#(parameter width = 48)(
         end
     end
     
-    parameter cmd_length = 6;
-    reg [width-1:0] send_data_reg;
-    reg [width-1:0] receive_data_reg;
     reg received_started;
     reg send_started;
     reg [9:0] counter_3;
     reg [9:0] compare_reg;
     reg wait_data;
     reg send_data;
-    reg start_2;
+//    reg start_2;
     reg [3:0] i ;
     reg [3:0] t ;
     
@@ -110,15 +105,13 @@ module SPI_1#(parameter width = 48)(
             MOSI <= 1'b1;
             
             state <= 3'b0;
-            send_data_reg <= {(width){1'b0}};
-            receive_data_reg <= {(width){1'b0}};
             counter_3 <= 10'b0;
             received_started <= 1'b0;
             compare_reg <= 10'b0;
             wait_data <= 1'b0;
             send_data <= 1'b0;
             send_started <= 1'b0;
-            start_2 <= 1'b0;
+//            start_2 <= 1'b0;
             
             busy_spi <= 1'b0;
             valid_spi <= 1'b0;
@@ -133,40 +126,76 @@ module SPI_1#(parameter width = 48)(
                     valid_response <= 1'b0;
                     valid_spi <= 1'b0;
                     wait_data <= 1'b0;
+                    send_data <= 1'b0;
+                    //////////////////////////////
+                    //For manually buffer data into FIFO
+                    //////////////////////////////
+                    if (t==0)begin
+                        wr_en_fifo <= 1'b1;
+                        wr_data_fifo <= 8'b01100111;
+                        t <= t+1;
+                    end
+                    else if (t==1)begin
+                        wr_en_fifo <= 1'b1;
+                        wr_data_fifo <= 8'b11100111;
+                        t <= t+1;
+                    end
+                    else if (t==2)begin
+                        wr_en_fifo <= 1'b1;
+                        wr_data_fifo <= 8'b01100110;
+                        t <= t+1;
+                    end
+                    else if (t==3)begin
+                        wr_en_fifo <= 1'b1;
+                        wr_data_fifo <= 8'b01100111;
+                        t <= t+1;
+                    end
+                    else if (t==4)begin
+                        wr_en_fifo <= 1'b1;
+                        wr_data_fifo <= 8'b01010101;
+                        t <= t+1;
+                    end
+                    else if (t==5)begin
+                        wr_en_fifo <= 1'b1;
+                        wr_data_fifo <= 8'b01010101;
+                        t <= t+1;
+                    end
+                    else begin
+                        wr_en_fifo <= 1'b0;
+                    end
                     if(spi_start)begin
                         state <= SEND_CMD_AND_DATA;
                         CS <= 1'b0;
                         busy_spi <= 1'b1;
+                        rd_en_fifo <= 1'b1;
                     end
                 end
                 SEND_CMD_AND_DATA:begin
-                    compare_reg <= (!send_data) ? (cmd_length-1) : (send_data_length-1);  // daha sonra send_data_length -1 mi olmal? diye bak
+                    compare_reg <= (!send_data) ? (cmd_length-1) : (send_data_length-1);
                     send_started <= (counter_3==0) ? (1'b1) : (send_started);
-                    rd_en_fifo <= (counter_3==0) ? (1'b1) : (1'b0);
+                    rd_en_fifo <= (rd_en_fifo) ? (1'b0) : (1'b0);
                     if (send_started)begin
-                        start_2 <= 1'b1;
-                        if((counter_3<=compare_reg)&& start_2)begin
+                        if((counter_3<=compare_reg))begin
                             MOSI <= rd_data_fifo[7-i];
                             if(i == 7) begin
-                                rd_en_fifo <= 1'b1;
                                 counter_3 <= counter_3 + 1;
                                 i <= 0;
                             end else begin
+                                rd_en_fifo <= (i == 6) ? (1'b1) : (1'b0);
                                 i <= i + 1;
-                                rd_en_fifo <= 1'b0;
                             end
                         end
                         else begin
-                            state <= (start_2) ? (WAIT_RESPONSE_AND_DATA) : (SEND_CMD_AND_DATA);
+                            state <= (!send_data) ? (WAIT_RESPONSE_AND_DATA) : (VALID_DATA);
                             counter_3 <= 10'b0;
-                            send_data <= 1'b0;
                             send_started <= 1'b0;
+                            rd_en_fifo <= 1'b0;
                         end
                     end
                 end
                 WAIT_RESPONSE_AND_DATA:begin
                     received_started <= ((!MISO && !received_started)) ? (1'b1) : (received_started);
-                    compare_reg <= (!wait_data) ? (response_length-1) : (receive_data_length);
+                    compare_reg <= (!wait_data) ? (response_length-1) : (receive_data_length-1);
                     if(received_started)begin
                         if (counter_3<=compare_reg)begin
                             wr_data_fifo[7-i] <= MISO;
@@ -183,26 +212,31 @@ module SPI_1#(parameter width = 48)(
                             wr_en_fifo <= 0;
                             received_started <= 1'b0;
                             counter_3 <= 10'b0;
-                            ////////////////////////////
-                            // burda bi hata var receive 0 olunca send data ya geçmeden valid yap?yo buraya bi ayar çek
-                            //////////////////////////////
+                            valid_response <= 1'b1;
                             if(receive_data_length==0)begin
-                                state <= VALID_DATA;
+                                if(send_data_length==0)begin
+                                    state <= VALID_DATA;
+                                end
+                                else begin
+                                    state <= SEND_CMD_AND_DATA;
+                                    send_data <= 1'b1;
+                                    rd_en_fifo <= 1'b1;
+                                end
                             end
                             else if (wait_data) begin 
                                 state <= VALID_DATA;
                                 wait_data <= 1'b0;
                             end
                             else begin
-                                if(!cmd_type)begin
+                                if(send_data_length==0)begin
                                     state <= WAIT_RESPONSE_AND_DATA;
                                     wait_data <= 1'b1;
                                 end
                                 else begin
                                     state <= SEND_CMD_AND_DATA;
+                                    rd_en_fifo <= 1'b1;
                                     send_data <= 1'b1;
                                 end
-                                valid_response <= 1'b1;
                             end
                         end
                     end 
