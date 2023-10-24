2d4a8b48f6f2181ab82dcd6138eefb54f80f8684
cimonik <142085739+cimonik@users.noreply.github.com>
2023-10-24 17:23:21 +0300

    Update comparator1.v

--- ALU/IEEE_ADDER_LAST_SEQU/comparator1.v
+++ ALU/IEEE_ADDER_LAST_SEQU/comparator1.v
@@ -1,4 +1,3 @@
-`timescale 1ns / 1ps
 module comparator #(parameter width=8)(
     input [31:0] X,
     input [31:0] Y,
@@ -7,24 +6,15 @@ module comparator #(parameter width=8)(
     output [31:0] outL
     );
     wire [8:0] exp1,exp2;
-    wire [23:0] mantis1,mantis2;
     wire [8:0] diffexp;
-    wire [23:0] diffmantis;
-    wire control;
+    
     assign exp1 = {1'b0,X[30:23]};
     assign exp2 = {1'b0,Y[30:23]};
-    assign mantis1 ={1'b0,X[22:0]};
-    assign mantis2 ={1'b0,Y[22:0]};
-    
     
     assign diffexp = exp1-exp2;
-    assign diffmantis = mantis1-mantis2;
-    assign control = (diffexp==0)&& diffmantis[23];
     
-    assign outB = (diffexp[8]|| control) ? (Y) : (X);    
-    assign outL = (diffexp[8]|| control) ? (X) : (Y);  
+    assign outB = (diffexp[8]) ? (Y) : (X);    
+    assign outL = (diffexp[8]) ? (X) : (Y);  
     assign dif  = (diffexp[8]) ?  (~diffexp+1):(diffexp)    ;
-    
-    
     
 endmodule
