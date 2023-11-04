613494c403694828f39e42a248bbe7e3e527035b
cimonik <142085739+cimonik@users.noreply.github.com>
2023-11-04 20:20:53 +0300

    Update adder.v

--- IEEE_754_Single_Precision/Adder/adder.v
+++ IEEE_754_Single_Precision/Adder/adder.v
@@ -77,3 +77,64 @@ endcase
     assign out_wire[30:23] = sign ? exp_inc+~{3'd0,count} :exp_mux;
     assign out_wire[31] = outB[31];
 endmodule
+
+module leading(
+    input [23:0] data,
+    output reg [4:0] count
+);
+
+always @(*) begin
+        if       (data[23]) count =  5'd0;
+        else if  (data[22]) count =  5'd1;
+        else if  (data[21]) count =  5'd2;
+        else if  (data[20]) count =  5'd3;
+        else if  (data[19]) count =  5'd4;
+        else if  (data[18]) count =  5'd5;
+        else if  (data[17]) count =  5'd6;
+        else if  (data[16]) count =  5'd7;
+        else if  (data[15]) count =  5'd8;
+        else if  (data[14]) count =  5'd9;
+        else if  (data[13]) count = 5'd10;
+        else if  (data[12]) count = 5'd11;
+        else if  (data[11]) count = 5'd12;
+        else if  (data[10]) count = 5'd13;
+        else if  (data[9])  count = 5'd14;
+        else if  (data[8])  count = 5'd15;
+        else if  (data[7])  count = 5'd16;
+        else if  (data[6])  count = 5'd17;
+        else if  (data[5])  count = 5'd18;
+        else if  (data[4])  count = 5'd19;
+        else if  (data[3])  count = 5'd20;
+        else if  (data[2])  count = 5'd21;
+        else if  (data[1])  count = 5'd22;
+        else count = 5'd23;
+    end
+endmodule
+
+module comparator #(parameter width=8)(
+    input [31:0] X,
+    input [31:0] Y,
+    output [8:0] dif,
+    output [31:0] outB,
+    output [31:0] outL
+    );
+    wire [8:0] exp1,exp2;
+    wire [23:0] mantis1,mantis2;
+    wire [8:0] diffexp;
+    wire [23:0] diffmantis;
+    wire control;
+    assign exp1 = {1'b0,X[30:23]};
+    assign exp2 = {1'b0,Y[30:23]};
+    assign mantis1 ={1'b0,X[22:0]};
+    assign mantis2 ={1'b0,Y[22:0]};
+    
+    
+    assign diffexp = exp1-exp2;
+    assign diffmantis = mantis1-mantis2;
+    assign control = (diffexp==0)&& diffmantis[23];
+    
+    assign outB = (diffexp[8]|| control) ? (Y) : (X);    
+    assign outL = (diffexp[8]|| control) ? (X) : (Y);  
+    assign dif  = (diffexp[8]) ?  (~diffexp+1):(diffexp)    ;
+
+endmodule
